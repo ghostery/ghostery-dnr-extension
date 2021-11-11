b663f63ba0a364d42dfaa6838f4ecc18e432ef08
Krzysztof Jan Modras <chrmod@chrmod.net>
2021-11-11 18:24:09 +0100

    Mobile UI

--- src/content_scripts/wtm-report/index.js
+++ src/content_scripts/wtm-report/index.js
@@ -11,24 +11,34 @@
 
 const WRAPPER_CLASS = 'wtm-popup-iframe-wrapper';
 
+const isMobile = window.navigator.userAgent.match(/iPhone/i);
+
 function closePopups() {
   [...document.querySelectorAll(`.${WRAPPER_CLASS}`)].forEach(popup => {
     popup.parentElement.removeChild(popup);
   });
 }
 
+const getTop = el => el.offsetTop + (el.offsetParent && getTop(el.offsetParent));
+
 function renderPopup(container, stats) {
   closePopups()
 
   const wrapper = document.createElement('div');
   wrapper.classList.add(WRAPPER_CLASS);
-  wrapper.style.left = container.getBoundingClientRect().width + 15 + 'px';
+  if (isMobile) {
+    wrapper.style.width = window.innerWidth - 20 + 'px';
+    wrapper.style.left = '10px';
+  } else {
+    wrapper.style.left = container.getBoundingClientRect().left - (350/2) + 12 + 'px';
+  }
+  wrapper.style.top = getTop(container) + 25 + 'px';
 
   const iframe = document.createElement('iframe');
   iframe.setAttribute('src', chrome.runtime.getURL(`wtm-report/index.html?domain=${stats.domain}`));
 
   wrapper.appendChild(iframe);
-  container.appendChild(wrapper);
+  document.body.appendChild(wrapper);
 }
 
 function renderWheel(anchor, stats) {
@@ -44,12 +54,12 @@ function renderWheel(anchor, stats) {
 
   const container = document.createElement('div');
   container.classList.add('wtm-tracker-wheel-container');
-  if (window.navigator.userAgent.match(/iPhone/i)) {
+  if (isMobile) {
     container.style.left = threeDotsElement.getBoundingClientRect().left - parent.getBoundingClientRect().left - 50 + 'px';
+    container.style.top = 15 + 'px';
   } else {
     container.style.left = threeDotsElement.getBoundingClientRect().right - parent.getBoundingClientRect().left + 5 + 'px';
   }
-  // container.style.top = threeDotsElement.getBoundingClientRect().top - parent.getBoundingClientRect().top + 7 + 'px';
 
   container.addEventListener('click', (ev) => {
     renderPopup(container, stats);
