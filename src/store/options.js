466935e996aa9518a992389facdb4295fbff84ed
Dominik Lubański <dominik.lubanski@gmail.com>
2022-04-19 10:38:17 +0200

    Use observe method to sync dnr lists

--- src/store/options.js
+++ src/store/options.js
@@ -24,65 +24,37 @@ export const DNR_IDS = chrome.runtime
 const Options = {
   trackerWheelDisabled: false,
   wtmSerpReport: true,
-  dnr: DNR_IDS.reduce((all, rule) => ({ ...all, [rule]: false }), {}),
+  dnr: DNR_IDS.reduce((all, rule) => ({ ...all, [rule]: true }), {}),
   [store.connect]: {
     async get() {
       const { options = {} } = await chrome.storage.local.get(['options']);
-
-      const enabledRulesetIds =
-        await chrome.declarativeNetRequest.getEnabledRulesets();
-
-      // Not yet saved, use values from the manifest.
-      if (!options.dnr) {
-        options.dnr = enabledRulesetIds.reduce((dnr, type) => {
-          dnr[type] = true;
-          return dnr;
-        }, {});
-      } else {
-        // Ensure that DNR rulesets are equal to those from options.
-        // eg. when web extension updates, the rulesets are reset
-        // to the value from the manifest.
-        Object.keys(options.dnr).forEach((rule) => {
-          const enabled = options.dnr[rule];
-          if (enabled !== enabledRulesetIds.includes(rule)) {
-            chrome.declarativeNetRequest.updateEnabledRulesets({
-              [enabled ? 'enableRulesetIds' : 'disableRulesetIds']: [rule],
-            });
-          }
-        });
-      }
-
       return options;
     },
-    async set(_, options, keys) {
-      const prevOptions = await this.get();
-
-      options = {
-        ...options,
-        ...Object.fromEntries(keys.map((key) => [key, options[key]])),
-      };
-
-      // Update declarativeNetRequest rulesets
-      if (keys.includes('dnr')) {
-        DNR_IDS.forEach((rule) => {
-          const enabled = options.dnr[rule];
-          if (enabled !== prevOptions.dnr[rule]) {
-            chrome.declarativeNetRequest.updateEnabledRulesets({
-              [enabled ? 'enableRulesetIds' : 'disableRulesetIds']: [rule],
-            });
-          }
-        });
-      }
-
-      // Save options to the local storage
+    async set(_, options) {
       await chrome.storage.local.set({ options });
-
+      return options;
+    },
+    async observe(id, options) {
       // Send update message to another contexts (background page / panel / options)
       chrome.runtime.sendMessage({
         action: UPDATE_ACTION_NAME,
       });
 
-      return options;
+      // Ensure that DNR rulesets are equal to those from options.
+      // eg. when web extension updates, the rulesets are reset
+      // to the value from the manifest.
+
+      const enabledRulesetIds =
+        await chrome.declarativeNetRequest.getEnabledRulesets();
+
+      DNR_IDS.forEach((rule) => {
+        const enabled = options.dnr[rule];
+        if (enabled !== enabledRulesetIds.includes(rule)) {
+          chrome.declarativeNetRequest.updateEnabledRulesets({
+            [enabled ? 'enableRulesetIds' : 'disableRulesetIds']: [rule],
+          });
+        }
+      });
     },
   },
 };
