c5ebd8ef40957324420ef7920c2df2b549bc706a
Dominik Lubański <dominik.lubanski@gmail.com>
2022-07-07 13:58:03 +0200

    Fix checkbox access by keyboard

--- src/pages/onboarding/components/checkbox.js
+++ src/pages/onboarding/components/checkbox.js
@@ -21,26 +21,35 @@ export default define({
     <ui-icon name="check" hidden="${!checked}"></ui-icon>
   `.css`
     :host {
+      position: relative;
       display: flex;
       align-items: center;
       justify-content: center;
-      box-sizing: border-box;
       width: 44px;
       height: 44px;
+    }
+
+    ::slotted(input) {
+      position: absolute;
+      inset: 0;
+      appearance: none;
+      box-sizing: border-box;
       background: var(--ui-color-gray-800);
       border: 2px solid var(--ui-color-primary-500);
       box-shadow: inset 4px 4px 8px rgba(0, 0, 0, 0.3);
       border-radius: 22px;
+      padding: 0;
+      margin: 0;
     }
 
-    :host([checked]) {
+    :host([checked]) ::slotted(input) {
       border-color: transparent;
       box-shadow: none;
       background: var(--ui-color-primary-500);
     }
 
-    div {
-      display: none;
+    ui-icon {
+      z-index: 1;
     }
   `,
 });
